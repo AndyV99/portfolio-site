08fea193820618b573aae0da1c0767708c4deed4
Andy <andyvorndran99@gmail.com>
2024-04-26 17:29:25 -0400

    initial work on landing page

---
+++ portfolio-site/src/views/Landing.vue
@@ -0,0 +1,103 @@
+<template>
+    <main class="container">
+        <h1 id="intro">Hi, I'm <span class='red'>Andy</span></h1>
+        <div class="textscrollbox"><h1 class="dev-text">Web Developer</h1></div>
+        <div class="textscrollbox"><h1 class="eng-text">Software Engineer</h1></div>
+
+        <div class="navbox">
+            <RouterLink to='/'>Home</RouterLink>
+            <RouterLink to='/about'>About Me</RouterLink>
+            <RouterLink to='/experience'>Work Experience</RouterLink>
+            <RouterLink to='/demo-projects'>Demo Projects</RouterLink>
+            <RouterLink to='/site-info'>Site Info</RouterLink>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+.v-leave-active {
+    transition: scroll-up 0.5s ease;
+}
+
+.v-leave-from {
+    margin-bottom: 0;
+}
+
+.v-leave-to {
+    margin-bottom: 1200px;
+}
+
+@keyframes scroll-up {
+    0% {margin-bottom:0;}
+    100% {margin-bottom:1200px;}
+}
+
+.red {
+    color: red;
+}
+.navbox {
+    display: grid;
+    text-align: right;
+    position: absolute;
+    bottom: 64px;
+    right: 64px;
+    overflow: hidden;
+}
+
+a {
+    font-family: 'Josefin Sans';
+    font-size: 3vw;
+}
+
+main {
+    margin: 16px;
+    margin-top: 32px;
+}
+
+#intro {
+    font-size: 8vw;
+    line-height: 8vw;
+    opacity: 0;
+    animation: fadein 2s;
+    animation-fill-mode: forwards;
+    padding-bottom: 4px;
+    border-bottom: 3px solid white;
+    margin-bottom: 16px;
+}
+
+h1 {
+    font-size: 6vw;
+    font-family: 'Josefin Sans';
+    display: inline-block;
+    line-height: 6.5vw;
+}
+
+.textscrollbox {
+    overflow: hidden;
+    height: 7vw;
+    margin: 16px;
+}
+
+.dev-text {
+    margin-left: -2000px;
+    animation: scrollin 3s;
+    animation-fill-mode: forwards;
+}
+
+.eng-text {
+    margin-left: -2000px;
+    animation: scrollin 3.5s;
+    animation-delay: 1.5s;
+    animation-fill-mode: forwards;
+}
+
+@keyframes fadein {
+    0% {opacity:0;}
+    100% {opacity:1;}
+}
+
+@keyframes scrollin {
+    0% {margin-left: -2000px;}
+    100% {margin-left: 0px;}
+}
+</style>
