d0d06275c601b7ddf016f8c4363603397fef2770
Andy <andyvorndran99@gmail.com>
2024-04-29 16:25:55 -0400

    Update mobile scaling

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -4,6 +4,11 @@ import Nav from '@/components/Nav.vue'
 <template>
   <main>
     <h1>Work Experience</h1>
+    <div class="experience-box">
+      <h2>PS Software Development Engineer <i>Five9</i></h2>
+      <h3>January 2022 - Present</h3>
+      <p>What I learned at driving school was</p>
+    </div>
   </main>
   <Nav />
 </template>
